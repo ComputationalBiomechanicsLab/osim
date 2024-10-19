4a2833b9bed86df268a593c4be997bdd5d4c5808
Adam Kewley <contact@adamkewley.com>
2024-10-19 20:05:02 +0200

    Add gitignore, clean up CMakeLists.txt slightly

--- apps/example_app.cpp
+++ apps/example_app.cpp
@@ -1,17 +1,7 @@
-#include <OpenSim/Analyses/RegisterTypes_osimAnalyses.h>
-#include <OpenSim/Common/RegisterTypes_osimCommon.h>
-#include <OpenSim/Simulation/RegisterTypes_osimSimulation.h>
-#include <OpenSim/Tools/RegisterTypes_osimTools.h>
-#include <OpenSim/Actuators/RegisterTypes_osimActuators.h>
-#include <OpenSim/ExampleComponents/RegisterTypes_osimExampleComponents.h>
+#include <osim/osim.h>
 
 int main(int argc, char** argv)
 {
-    RegisterTypes_osimCommon();
-    RegisterTypes_osimSimulation();
-    RegisterTypes_osimActuators();
-    RegisterTypes_osimAnalyses();
-    RegisterTypes_osimTools();
-    RegisterTypes_osimExampleComponents();
+    osim::init();
     return 0;
 }
