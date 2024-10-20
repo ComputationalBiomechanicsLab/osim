4aa2934065b92c3f9d9229effa3e56d626aeeb0f
Adam Kewley <contact@adamkewley.com>
2024-10-20 14:54:23 +0200

    Add osim.h to intallation fileset

--- src/osim/osim.h
+++ src/osim/osim.h
@@ -5,18 +5,40 @@
 
 namespace osim
 {
+    // Runtime configuration that can be given to `osim::init` to change/monitor
+    // its behavior.
     class InitConfiguration final {
     public:
         virtual ~InitConfiguration() noexcept = default;
 
+        // Called when `osim::init` wants to write `content` as a warning message.
         void log_warning(std::string content) { impl_log_warning(std::move(content)); }
+
+        // Called when `osim::init` wants to write `content` as an info message.
         void log_info(std::string content) { impl_log_info(std::move(content)); }
+
     private:
-        virtual void impl_log_warning(std::string);  // defaults to writing to `std::cerr`
-        virtual void impl_log_info(std::string);     // defaults to writing to `std::cerr`
+
+        // Implementors can override this to provide custom warning message logging behavior.
+        //
+        // By default, it writes warning messages to `std::cerr`.
+        virtual void impl_log_warning(std::string);
+
+        // Implementors can override this to provide custom info message logging behavior.
+        //
+        // By default, it writes info messages to `std::cerr`.
+        virtual void impl_log_info(std::string);
     };
 
+    // Globally initializes the osim (OpenSim + extensions) API with a default `InitConfiguration`.
+    //
+    // This should be called by the application exactly once before using any `osim::`,
+    // `SimTK::`, or `OpenSim::`-prefixed API.
     void init();
+
+    // Globally initializes the osim (OpenSim + extensions) API with the given `InitConfiguration`.
+    //
+    // This should be called by the application exactly once before using any `osim::`,
+    // `SimTK::`, or `OpenSim::`-prefixed API.
     void init(InitConfiguration&);
 }
-
